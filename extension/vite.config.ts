e9d316ce3debd23a11a3bf87abe950d44a158f02
Henry Deutsch <henrymdeutsch@gmail.com>
2025-12-22 21:10:12 -0500

    build hot reloading!!!

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -1,37 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './public/manifest.json'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/manifest.json',
-          dest: '.',
-        }
-      ],
-    }),
+    crx({ manifest }),
     tailwindcss(),
   ],
   build: {
     outDir: 'build',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        background: './src/scripts/background.ts',
-        // We compile the content script ourselves to support module imports
-        // content: './src/scripts/content.ts',
-      },
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    },
+    emptyOutDir: false,   // Don't empty during watch builds
     minify: true,
     sourcemap: true,
   },
